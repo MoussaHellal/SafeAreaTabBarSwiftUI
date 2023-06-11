a21882046e6f426d4b730465b928a3ba33292662
Moussa Hellal <moussa@sane.agency>
2023-06-11 10:22:06 +0100

    Initial Commit

---
+++ SafeAreTabBar/SafeAreTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeAreTabBarApp.swift
+//  SafeAreTabBar
+//
+//  Created by Moussa on 11/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct SafeAreTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
